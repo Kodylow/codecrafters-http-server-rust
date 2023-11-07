dd8163a8d2915f12fd57fa8b926bd65e3c434f92
kody.low <kodylow7@gmail.com>
2023-11-07 08:48:01 -0800

    part 2

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 // Uncomment this block to pass the first stage
+use std::io::Write;
 use std::net::TcpListener;
 
-use tracing::info;
+use tracing::{error, info};
 
 // To try this locally on macOS:
 // run ./your_server.sh in one terminal session
@@ -10,19 +11,27 @@ use tracing::info;
 // -z just scan for listening daemons, without sending any data to them.)
 
 fn main() {
-    let subscriber = tracing_subscriber::fmt()
+    let tracer = tracing_subscriber::fmt()
         .with_max_level(tracing::Level::INFO)
         .finish();
-    tracing::subscriber::set_global_default(subscriber).unwrap();
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    info!("Logs from your program will appear here!");
+    tracing::subscriber::set_global_default(tracer).unwrap();
+    // info!("Logs from your program will appear here!");
 
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                // read data from connection
+                let mut buf = [0; 512];
+                stream.peek(&mut buf).unwrap();
+                // log the data
+                info!("Received data: {}", String::from_utf8_lossy(&buf[..]));
+                // send response HTTP/1.1 2002 OK\r\n\r\n
+                let response = "HTTP/1.1 200 OK\r\n\r\n";
+                if let Err(e) = stream.write(response.as_bytes()) {
+                    error!("error: {}", e);
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
