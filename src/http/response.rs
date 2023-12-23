0a4a23f0431c7a5bf1afd5eba2471bfde549afab
kody.low <kodylow7@gmail.com>
2023-12-23 11:01:46 -0800

    content length

--- src/http/response.rs
+++ src/http/response.rs
@@ -1,6 +1,7 @@
 pub struct Response {
     status_code: &'static str,
     content_type: &'static str,
+    content_length: usize,
     body: String,
 }
 
@@ -9,14 +10,15 @@ impl Response {
         Response {
             status_code,
             content_type,
+            content_length: body.len(),
             body,
         }
     }
 
     pub fn format(&self) -> String {
         format!(
-            "HTTP/1.1 {}\r\nContent-Type: {}\r\n\r\n{}",
-            self.status_code, self.content_type, self.body
+            "HTTP/1.1 {}\r\nContent-Type: {}\r\nContent-Length: {}\r\n\r\n{}",
+            self.status_code, self.content_type, self.content_length, self.body
         )
     }
 }
