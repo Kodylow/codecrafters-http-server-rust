c6cdeac8f421d3a00684d66ec9f61e68ff1df7bd
kody.low <kodylow7@gmail.com>
2023-12-23 11:11:24 -0800

    request parsing

--- src/main.rs
+++ src/main.rs
@@ -18,11 +18,46 @@ const OK: &str = "200 OK";
 const NOT_FOUND: &str = "404 NOT FOUND";
 const TEXT_PLAIN: &str = "text/plain";
 
+fn create_response(request: Request) -> Response {
+    match request.path.as_str() {
+        path if path.starts_with("/echo/") => {
+            let random_string = &path[6..];
+            Response::new(OK, TEXT_PLAIN, random_string.to_string())
+        }
+        path if path == "/" => Response::new(OK, TEXT_PLAIN, "".to_string()),
+        path if path == "/user-agent" => Response::new(OK, TEXT_PLAIN, request.user_agent),
+        _ => Response::new(NOT_FOUND, TEXT_PLAIN, "".to_string()),
+    }
+}
+
+fn parse_stream(stream: &mut std::net::TcpStream) -> Result<String> {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+    let request = String::from_utf8(buffer.to_vec())?;
+    info!("request stream: {}", request);
+    Ok(request)
+}
+
 fn handle_connection(mut stream: std::net::TcpStream) {
     let request = parse_stream(&mut stream).unwrap();
-    let parsed_request = Request::parse(&request).unwrap();
-    let response = create_response(parsed_request);
-    stream.write(response.format().as_bytes()).unwrap();
+    match Request::parse(&request) {
+        Ok(parsed_request) => {
+            let response = create_response(parsed_request);
+            stream.write(response.format().as_bytes()).unwrap();
+        }
+        Err(e) => {
+            eprintln!("Failed to parse request: {}", e);
+            // Handle the error appropriately here
+        }
+    }
+}
+
+fn tracing_setup() -> Result<(), anyhow::Error> {
+    let tracer = tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .finish();
+    tracing::subscriber::set_global_default(tracer)?;
+    Ok(())
 }
 
 fn main() -> Result<()> {
@@ -42,32 +77,4 @@ fn main() -> Result<()> {
     }
 
     Ok(())
-}
-
-fn tracing_setup() -> Result<(), anyhow::Error> {
-    let tracer = tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::INFO)
-        .finish();
-    tracing::subscriber::set_global_default(tracer)?;
-    Ok(())
-}
-
-fn parse_stream(stream: &mut std::net::TcpStream) -> Result<String> {
-    let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
-    let request = String::from_utf8(buffer.to_vec())?;
-    info!("request stream: {}", request);
-    Ok(request)
-}
-
-fn create_response(request: Request) -> Response {
-    match request.path.as_str() {
-        path if path.starts_with("/echo/") => {
-            let random_string = &path[6..];
-            Response::new(OK, TEXT_PLAIN, random_string.to_string())
-        }
-        path if path == "/" => Response::new(OK, TEXT_PLAIN, "".to_string()),
-        path if path == "/user-agent" => Response::new(OK, TEXT_PLAIN, request.user_agent),
-        _ => Response::new(NOT_FOUND, TEXT_PLAIN, "".to_string()),
-    }
 }
